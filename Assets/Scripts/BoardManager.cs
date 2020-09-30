25afe9530a4a2c645782ea846bf3cef49b2de200
Louise leal <lcloudine@gmail.com>
2020-09-30 14:34:42 -0400

    First commit

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+
+public class BoardManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private int boardSizeX = 8;
+    [SerializeField]
+    private int boardSizeY = 8;
+
+    [SerializeField]
+    private Transform boardContainer;
+
+    [SerializeField]
+    private Sprite backgroundTileSprite;
+
+    [SerializeField]
+    private Tile tilePrefab;
+    [SerializeField]
+    private Vector2 tileSize;
+
+
+    private Tile[,] board;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(tilePrefab != null)
+        {
+            tileSize = tilePrefab.GetSpriteBounds();
+        }
+
+        InitBoard();
+    }
+
+    private void InitBoard()
+    {
+        board = new Tile[boardSizeX, boardSizeY];
+        for(int x = 0 ; x < boardSizeX; x++)
+        {
+            for (int y = 0; y < boardSizeY; y++)
+            {
+                var newTile = Instantiate<Tile>(tilePrefab, boardContainer) as Tile;
+
+                newTile.SetPosition(x * tileSize.x, y * tileSize.y);
+                newTile.SetSprite(backgroundTileSprite);
+
+                board[x, y] = newTile;
+            }
+        }
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour
+{ 
+
+    public Vector2 position;
+    public Image image;
+
+    public RectTransform rect;
+
+    public void SetPosition(float positionX, float positionY)
+    {
+        rect.position = position;
+    }
+
+    public void SetSprite(Sprite sprite)
+    {
+        image.sprite = sprite;
+    }
+
+    public Vector2 GetSpriteBounds()
+    {
+        return rect.sizeDelta;
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if(rect == null)
+        {
+            rect = GetComponent<RectTransform>();
+        }
+        if (image == null)
+        {
+            image = GetComponent<Image>();
+        }
+    }
+#endif
+}
